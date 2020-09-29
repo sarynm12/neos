58048ee90696d3a239345ae907616347517412a9
Saryn Mooney <smooney15@my.whitworth.edu>
2020-09-29 15:42:21 -0600

    tests passing after refactor

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,25 +1,47 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+require 'json'
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
 
 class NearEarthObjects
   def self.find_neos_by_date(date)
+    @date = date
+    {
+      astroid_list: formatted_asteroid_data,
+      biggest_astroid: largest_astroid_diameter,
+      total_number_of_astroids: total_number_of_astroids
+    }
+  end
+
+  def self.conn
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
+      params: { start_date: @date, api_key: ENV['nasa_api_key']}
     )
+  end
+
+  def self.asteroids_list_data
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
+  end
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  def self.parsed_asteroids_data
+    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{@date}"]
+  end
 
+  def self.largest_astroid_diameter
     largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
+  end
 
+  def self.total_number_of_astroids
     total_number_of_astroids = parsed_asteroids_data.count
+  end
+
+  def self.formatted_asteroid_data
     formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
       {
         name: astroid[:name],
@@ -27,11 +49,5 @@ class NearEarthObjects
         miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
-
-    {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
-    }
   end
 end
